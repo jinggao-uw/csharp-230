03b6629bd2969d5b43350dff1c3dd64e9cdf0409
Jing Gao <Jing Gao>
2017-04-21 00:25:18 -0700

    Homework of BirthdayCard project

---
+++ BirthdayCard/BirthdayCard/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BirthdayCard.Controllers
+{
+    public class HomeController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+           return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Models.Users users)
+        {
+            if (ModelState.IsValid)
+            {
+               return View("SentCard", users);
+            }
+            else
+            {
+               return View();
+            }
+        }
+
+        public ActionResult SentCard()
+        {
+            return View();
+        }
+    }
+}
